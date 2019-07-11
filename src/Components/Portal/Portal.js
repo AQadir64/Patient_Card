a7577ff2f322a39232067618990512a9efcfe34c
Abdul Qadir <a.qadir67@outlook.com>
2019-07-11 14:42:36 +0500

    Portal of app

---
+++ src/Components/Portal/Portal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Portal extends Component {
+    render() {
+        console.log(this.props.user)
+        return (
+            <div>
+                Portalllll 
+            </div>
+        )
+    }
+}
+
+export default Portal
